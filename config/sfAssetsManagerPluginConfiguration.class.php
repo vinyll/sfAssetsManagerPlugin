d8f54b72f6e0e1aabed24824471d42dba4454142
Vincent Agnano <vincent.agnano@particul.es>
2010-12-08 17:20:19 +0100

    added autoload helper and its app config option

--- config/sfAssetsManagerPluginConfiguration.class.php
+++ config/sfAssetsManagerPluginConfiguration.class.php
@@ -5,5 +5,11 @@ class sfAssetsManagerPluginConfiguration extends sfPluginConfiguration
   {
     $this->dispatcher->connect('debug.web.load_panels', array('sfAssetsManagerDebugPanel', 'listenToLoadPanelEvent'));
     $this->dispatcher->connect('sfAssetsManagerPlugin.load_package', array('sfAssetsManagerDebugPanel', 'listenToLoadPackageEvent'));
+    
+    if(sfConfig::get('app_sf_assets_manager_plugin_autoload_helper', false))
+    {
+      $helpers = array_merge(sfConfig::get('sf_standard_helpers', array()), array('sfAssetsManager'));
+      sfConfig::set('sf_standard_helpers', $helpers);
+    }
   }
 }
